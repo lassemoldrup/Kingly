bfb58e437096d2bc6d0cd09efb8fe99dd21544f9
Lasse <lasse.moeldrup@gmail.com>
2020-05-02 12:23:45 +0200

    Update my_benchmark.rs

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -6,19 +6,12 @@ use arrayvec::ArrayVec;
 use crusty::move_gen::{gentest, init_non_sliders, init_magics};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut test = black_box(Position::new_default());
-    test.set("4k3/8/8/2Pp4/8/8/8/4K3 w - d6 0 1").unwrap();
-    let mut av = ArrayVec::<[Move; 256]>::new();
     init_non_sliders();
     init_magics();
-
-    c.bench_function("En passant", |b| b.iter(|| {
-        av = ArrayVec::<[Move; 256]>::new();
-        unsafe { gentest(&test, &mut av) }
-    }));
-
     let test = black_box(Position::new_default());
-    c.bench_function("Initial pawns", |b| b.iter(|| {
+    let mut av = ArrayVec::<[Move; 256]>::new();
+
+    c.bench_function("Gen all", |b| b.iter(|| {
         av = ArrayVec::<[Move; 256]>::new();
         unsafe { gentest(&test, &mut av) }
     }));
