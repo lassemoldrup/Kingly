e30f6c772ae798b5f8cc3e49b28161ac46787106
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-10-01 02:41:35 +0200

    Begin rewrite

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,16 +1,1 @@
-#![feature(let_chains)]
 
-#[macro_use]
-extern crate lazy_static;
-
-pub mod eval;
-pub mod fen;
-pub mod move_gen;
-pub mod move_list;
-pub mod position;
-pub mod search;
-pub mod square_map;
-pub mod tables;
-pub mod types;
-pub mod util;
-pub mod zobrist;
