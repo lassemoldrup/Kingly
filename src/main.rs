4c857620dda2b89015561b8cb6c128aaf98f92c0
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-10-01 16:38:52 +0200

    Basic UCI

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+mod uci;
+
+use std::io;
+
+use clap::{Parser, Subcommand};
+use uci::Uci;
+
+#[derive(Parser)]
+struct App {
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[command(arg_required_else_help = true)]
+    Perft { fen: String, depth: u8 },
+}
+
+fn main() -> io::Result<()> {
+    let app = App::parse();
+    match app.command {
+        Some(Command::Perft { fen, depth }) => println!("Perft {depth} {fen}"),
+        None => Uci::new_standard().repl()?,
+    }
+    Ok(())
+}
